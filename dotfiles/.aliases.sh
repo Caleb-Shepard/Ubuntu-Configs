7cb087f2774cf86f9dd2ad52a36732613b06acb9
Caleb Shepard <cshepard6055@floridapoly.edu>
2017-10-25 14:50:26 -0400

    Added .aliases.sh; update .zshrc

---
+++ dotfiles/.aliases.sh
@@ -0,0 +1,21 @@
+echo "Don't forget to check the mail."
+
+# collection of personal aliases
+# some alias
+
+# Example aliases
+alias zshconfig="mate ~/.zshrc"
+alias ohmyzsh="mate ~/.oh-my-zsh"
+
+# Weather in Boston
+alias weather='curl wttr.in/boston'
+
+# Export java path
+# export JAVA_HOME="/usr/libexec/java_home -v 1.8"
+
+# other
+alias ssh_ember='ssh $(whoami)@ember.hpc.lab'
+alias awful='~/Scripts/awful.sh'
+alias lca='lolcat'
+alias lcat='lolcat'
+echo "Aliases added."
