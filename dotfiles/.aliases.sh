ab3f9e45e33570510d917ea0ddecb2618d05c8dd
Caleb Shepard <10443116+Caleb-Shepard@users.noreply.github.com>
2022-01-16 17:07:28 -0500

    Update .aliases.sh

--- dotfiles/.aliases.sh
+++ dotfiles/.aliases.sh
@@ -1,21 +1,4 @@
-echo "Don't forget to check the mail."
-
-# collection of personal aliases
-# some alias
-
-# Example aliases
-alias zshconfig="mate ~/.zshrc"
-alias ohmyzsh="mate ~/.oh-my-zsh"
-
-# Weather in Boston
-alias weather='curl wttr.in/boston'
-
-# Export java path
-# export JAVA_HOME="/usr/libexec/java_home -v 1.8"
-
-# other
-alias ssh_ember='ssh $(whoami)@ember.hpc.lab'
-alias awful='~/Scripts/awful.sh'
-alias lca='lolcat'
-alias lcat='lolcat'
-echo "Aliases added."
+# *NIX Shell agnostic alias config file
+alias zshconfig="vim ~/.zshrc"
+alias ohmyzsh="vim ~/.oh-my-zsh"
+alias weather='curl wttr.in/tampa'
