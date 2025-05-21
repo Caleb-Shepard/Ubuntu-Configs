97baedbed51b3fe3f1a885cfb75f80b0e1efd082
Caleb Shepard <10443116+Caleb-Shepard@users.noreply.github.com>
2025-05-21 10:45:53 -0400

    Add Shelby Mustangs

--- options/wallpapers.sh
+++ options/wallpapers.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 mkdir -p ~/Pictures/Wallpapers
+# Landscapes
 wget --user-agent="Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" "https://images.pexels.com/photos/1323550/pexels-photo-1323550.jpeg?cs=srgb&dl=pexels-simon-berger-1323550.jpg&fm=jpg&w=4608&h=2963" -P ~/Pictures/Wallpapers
 wget --user-agent="Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" "https://images.pexels.com/photos/2387873/pexels-photo-2387873.jpeg?cs=srgb&dl=pexels-sam-kolder-2387873.jpg&fm=jpg&w=5472&h=3648" -P ~/Pictures/Wallpapers
+# Shelby Mustangs
+wget --user-agent="Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" "https://media.wired.com/photos/5c3d01aec2835a2c29c0dc17/master/w_1600,c_limit/05_FordMustang.jpg" -P ~/Pictures/Wallpapers
+wget --user-agent="Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" "https://hips.hearstapps.com/hmg-prod/images/2022-ford-mustang-shelby-gt500-25-1636735299.jpg?crop=1xw:1xh;center,top&resize=980:*" -P ~/Pictures/Wallpapers
